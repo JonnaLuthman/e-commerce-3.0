a51df8dfcc12114bebce1b909336da60c201f2f3
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-04-06 16:43:34 +0100

    Restore index.ts with express server setup and middleware configuration

--- ecommerce-api-new/src/index.ts
+++ ecommerce-api-new/src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {connectDB} from "./src/config/db";
+import {connectDB} from "./config/db";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { VercelRequest, VercelResponse } from "@vercel/node";
@@ -18,11 +18,11 @@ app.use(cors({
 }));
 
 // Routes
-import productRouter from "./src/routes/products";
-import customerRouter from "./src/routes/customers";
-import orderRouter from "./src/routes/orders";
-import orderItemRouter from "./src/routes/orderItems";
-import stripeRouter from "./src/routes/stripe";
+import productRouter from "./routes/products";
+import customerRouter from "./routes/customers";
+import orderRouter from "./routes/orders";
+import orderItemRouter from "./routes/orderItems";
+import stripeRouter from "./routes/stripe";
 // import authRouter from "./routes/auth";
 app.use('/products', productRouter)
 app.use('/customers', customerRouter)
