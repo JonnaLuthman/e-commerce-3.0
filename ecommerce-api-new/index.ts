cfa58adbfebe03ea2631e02c45dbcb627fa8f8e2
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-04-06 17:23:13 +0200

    Trying to fix errors.

--- ecommerce-api-new/index.ts
+++ ecommerce-api-new/index.ts
@@ -1,7 +1,5 @@
 import express from "express";
-import bodyParser from 'body-parser';
-import { webhook } from "../src/controllers/stripeController";
-import {connectDB} from "../src/config/db";
+import {connectDB} from "./src/config/db";
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { VercelRequest, VercelResponse } from "@vercel/node";
@@ -9,12 +7,6 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-
-app.post(
-  "/stripe/webhook",
-  bodyParser.raw({ type: "application/json" }),
-  webhook
-);
 
 // Middleware
 app.use(express.json())
@@ -26,11 +18,11 @@ app.use(cors({
 }));
 
 // Routes
-import productRouter from "../src/routes/products";
-import customerRouter from "../src/routes/customers";
-import orderRouter from "../src/routes/orders";
-import orderItemRouter from "../src/routes/orderItems";
-import stripeRouter from "../src/routes/stripe";
+import productRouter from "./src/routes/products";
+import customerRouter from "./src/routes/customers";
+import orderRouter from "./src/routes/orders";
+import orderItemRouter from "./src/routes/orderItems";
+import stripeRouter from "./src/routes/stripe";
 // import authRouter from "./routes/auth";
 app.use('/products', productRouter)
 app.use('/customers', customerRouter)
--- ecommerce-api-new/src/routes/stripe.ts
+++ ecommerce-api-new/src/routes/stripe.ts
@@ -4,6 +4,6 @@ import { checkoutSession, webhook } from '../controllers/stripeController';
 const router = express.Router()
 
 router.post('/checkout', checkoutSession)
-// router.post("/webhook", bodyParser.raw({ type: "application/json" }), webhook);
+router.post("/webhook", bodyParser.raw({ type: "application/json" }), webhook);
 
 export default router;
