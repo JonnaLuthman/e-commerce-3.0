5d08e391a7a3963107b5b317a25718c1e41760f4
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-04-08 17:31:54 +0100

    New style to productCard.

--- ecommerce-client/src/components/Start/ProductCard.tsx
+++ ecommerce-client/src/components/Start/ProductCard.tsx
@@ -20,40 +20,28 @@ const ProductCard = ({ product }: ShowProductCardProps) => {
   };
 
   return (
-    <section className="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-      <div>
+    <section className="h-[70vh] flex flex-col border-b border-r bg-[#dfe0df]">
+      <div className="flex-1 bg-white">
         <Link
           to={`/product/${id}`}
-          className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
+          className="relative mx-3 mt-3 flex h-60 overflow-hidden"
         >
           <img
-            className="peer absolute top-0 right-0 h-full w-full object-cover"
+            className="flex-1 peer absolute top-0 right-0 h-full w-full object-cover"
             src={image}
             alt={name}
             width="282"
           />
         </Link>
-        <h2 className="text-xl tracking-tight text-slate-900">{name}</h2>
-        <div>
-          <p className="text-2xl font-bold text-slate-900">{price} sek</p>
-        </div>
       </div>
+      <div className="h-[8rem] py-[1.5rem]">
+        <h2 className="text-m tracking-tight text-slate-900]">{name}</h2>
+          <p className="text-m font-bold text-slate-900">{price} sek</p>
+        </div>
       <button
-        className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
+        className="mt-auto px-5 py-2.5 text-center text-sm font-medium text-[#e0dfdf] hover:bg-black cursor-pointer h-[3rem]"
         onClick={() => handleAddToCart(product, 1)}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="mr-2 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth="2"
-        >
-          <path
-            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-          />
-        </svg>
         Add to cart
       </button>
     </section>
