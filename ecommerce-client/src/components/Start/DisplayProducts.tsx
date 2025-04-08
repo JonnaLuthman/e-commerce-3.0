555fa22d228dc7b9f23d425d8d0fc8e04df920bc
Jonna Luthman <jonna.luthman@medieinstitutet.se>
2025-04-08 17:30:56 +0100

    Added pagination to displayProducts-comp.

--- ecommerce-client/src/components/Start/DisplayProducts.tsx
+++ ecommerce-client/src/components/Start/DisplayProducts.tsx
@@ -1,17 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ProductCard from "./ProductCard";
 import ProductContext from "../../contexts/ProductContext";
+import { Pagination } from "../../utils/Pagination";
 
 export const DisplayProducts = () => {
   const { products } = useContext(ProductContext);
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(9);
+
+  const indexOfLastOrder = currentPage * productsPerPage;
+  const indexOfFirstOrder = indexOfLastOrder - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstOrder, indexOfLastOrder);
+
   return (
     <>
-      <section className="products-wrapper">
-        {products.map((product) => (
+      <section className="grid grid-cols-2 md:grid-cols-3 border-t border-l">
+        {currentProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </section>
+      <Pagination
+        itemsPerPage={productsPerPage}
+        totalItems={products.length}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
     </>
   );
 };
